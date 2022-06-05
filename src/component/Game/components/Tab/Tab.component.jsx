173c8993c4b6e183c1150af6f12d2fb3356dd675
xango2080 <xango2080@users.noreply.github.com>
2022-06-05 22:04:00 +0200

    fix new word & update deps

--- src/component/Game/components/Tab/Tab.component.jsx
+++ src/component/Game/components/Tab/Tab.component.jsx
@@ -4,77 +4,75 @@ import GameContext from "../../context/Game.context";
 import classnames from "classnames";
 
 const CreateTd = memo(({line, currentTest, proposal, wordSplitted, letterCounter}) => {
+	const proposalSplitted = proposal && [...proposal];
 
-    const proposalSplitted = proposal && [...proposal];
+	if (proposalSplitted) {
+		for (let i = 0; i < wordSplitted.length; i++) {
+			if (wordSplitted[i] === proposalSplitted[i]) {
+				letterCounter[proposalSplitted[i]]--;
+			}
+		}
+	}
 
-    if (proposalSplitted) {
-        for (let i = 0; i < wordSplitted.length; i++) {
-            if (wordSplitted[i] === proposalSplitted[i]) {
-                letterCounter[proposalSplitted[i]]--;
-            }
-        }
-    }
+	return wordSplitted.map((letter, i) => {
+		let child = "";
 
+		if (i === 0 && (currentTest === line || proposalSplitted && proposalSplitted[0] === "-")) {
+			child = wordSplitted[0];
+		} else if (proposalSplitted && proposalSplitted[i] && proposalSplitted[i] !== "-") {
+			child = proposalSplitted[i];
+		}
 
-    return wordSplitted.map((letter, i) => {
-        let child = "";
+		const isgood = proposalSplitted && letter === proposalSplitted[i];
 
-        if (i === 0 && (currentTest === line || proposalSplitted && proposalSplitted[0] === "-")) {
-            child = wordSplitted[0];
-        } else if (proposalSplitted && proposalSplitted[i] && proposalSplitted[i] !== "-") {
-            child = proposalSplitted[i];
-        }
+		let notPlaced = false;
+		if (proposalSplitted && letter !== proposalSplitted[i] && wordSplitted.includes(proposalSplitted[i]) && letterCounter[proposalSplitted[i]] > 0) {
+			notPlaced = true;
+			letterCounter[proposalSplitted[i]]--;
+		}
 
-        const isgood = proposalSplitted && letter === proposalSplitted[i];
-
-        let notPlaced = false;
-        if (proposalSplitted && letter !== proposalSplitted[i] && wordSplitted.includes(proposalSplitted[i]) && letterCounter[proposalSplitted[i]] > 0) {
-            notPlaced = true;
-            letterCounter[proposalSplitted[i]]--;
-        }
-
-        return (<td
-                key={i}
-                className={classnames("tab__item", {
-                    "tab__item--find": isgood,
-                    "tab__item--not-placed": notPlaced,
-                    "tab__item--error": proposalSplitted && proposalSplitted[i] === "-",
-                })
-                }
-            >
-                {child}
-            </td>
-        )
-    })
-}, (prev, next) => prev.proposal === next.proposal);
+		return (<td
+				key={i}
+				className={classnames("tab__item", {
+					"tab__item--find": isgood,
+					"tab__item--not-placed": notPlaced,
+					"tab__item--error": proposalSplitted && proposalSplitted[i] === "-",
+				})
+				}
+			>
+				{child}
+			</td>
+		)
+	})
+}, (prev, next) => prev.wordSplitted === next.wordSplitted && prev.proposal === next.proposal);
 
 
 export const Tab = () => {
-    const {currentTest, nbTest, word, proposal} = useContext(GameContext);
-    const wordSplitted = useMemo(() => [...word], [word]);
+	const {currentTest, nbTest, word, proposal} = useContext(GameContext);
+	const wordSplitted = useMemo(() => [...word], [word]);
 
-    const letterCounter = useMemo(() => wordSplitted.reduce(
-        (res, char) => (res[char] = (res[char] || 0) + 1, res),
-        {}
-    ), [wordSplitted]);
+	const letterCounter = useMemo(() => wordSplitted.reduce(
+		(res, char) => (res[char] = (res[char] || 0) + 1, res),
+		{}
+	), [wordSplitted]);
 
-    return (
-        <table className="tab">
-            <tbody>
-            {[...Array(nbTest)].map((e, i) => (
-                <tr key={i}>
-                    <CreateTd
-                        line={i}
-                        currentTest={currentTest}
-                        proposal={proposal[i]}
-                        wordSplitted={wordSplitted}
-                        letterCounter={{...letterCounter}}
-                    />
-                </tr>
-            ))}
-            </tbody>
-        </table>
-    )
+	return (
+		<table className="tab">
+			<tbody>
+			{[...Array(nbTest)].map((e, i) => (
+				<tr key={i}>
+					<CreateTd
+						line={i}
+						currentTest={currentTest}
+						proposal={proposal[i]}
+						wordSplitted={wordSplitted}
+						letterCounter={{...letterCounter}}
+					/>
+				</tr>
+			))}
+			</tbody>
+		</table>
+	)
 };
 
 export default Tab;
